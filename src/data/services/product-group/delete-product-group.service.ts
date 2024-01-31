a6425469d0d1edc37a8cb80c1feb6fe81a034d76
Silas Carmo <augusto.fernandes775@gmail.com>
2024-01-30 22:01:10 -0300

    feat: allow deleting product group only if no product is associated with it

--- src/data/services/product-group/delete-product-group.service.ts
+++ src/data/services/product-group/delete-product-group.service.ts
@@ -4,20 +4,32 @@ import { DeleteProductGroup } from 'src/domain/services/product-group';
 import {
   RequiredFieldError,
   NotFoundProductGroupError,
+  ProductGroupWithProductsError,
 } from 'src/domain/errors';
+import { ProductRepository } from 'src/data/protocols/repositories/product';
 
 @Injectable()
 export class DeleteProductGroupService implements DeleteProductGroup {
   constructor(
     @Inject('ProductGroupRepository')
     private readonly productGroupRepository: ProductGroupRepository,
+    @Inject('ProductRepository')
+    private readonly productRepository: ProductRepository,
   ) { }
 
   async delete(input: DeleteProductGroup.Input): Promise<void> {
     if (!input.id) throw new RequiredFieldError('id');
 
-    const exist = await this.productGroupRepository.findById({ id: input.id });
-    if (!exist) throw new NotFoundProductGroupError();
+    const productGroup = await this.productGroupRepository.findById({
+      id: input.id,
+    });
+    if (!productGroup) throw new NotFoundProductGroupError();
+
+    const products = await this.productRepository.listAllByProductGroupId({
+      productGroupId: input.id,
+    });
+
+    if (products.length) throw new ProductGroupWithProductsError();
 
     await this.productGroupRepository.delete(input);
   }
